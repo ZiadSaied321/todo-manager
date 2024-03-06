8bcf6c7b0920d376f674bdccbcd9f25d69941d73
Ziad Saied <ziadsaied@gmail.com>
2024-03-06 22:52:07 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='container mx-auto text-center bg-slate-100 text-blue-950'>MY TODO APP</div>
+  )
+}
+
+export default App
